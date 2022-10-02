4085758cfb00c93fedb0643dab2d7198eb7e5ccd
tsemkaloalena <tsemkaloalena@gmail.com>
2022-10-02 19:42:45 +0300

    Tests for method recordWord are described

---
+++ FieldOfDreams/src/test/java/GameTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+	private Game game = new Game();
+
+	@Test
+	public void whenWordIsRecorded_thanLettersAreCheckedAsNotGuessed() {
+
+	}
+
+	@Test
+	public void whenWordWithDoubleLettersIsRecorded_thanAllLettersAreAdded() {
+
+	}
+
+	@Test
+	public void whenUppercaseLettersAreInWord_thenConvertedToLowercase() {
+
+	}
+}
