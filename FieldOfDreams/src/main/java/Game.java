bd787bb62aa00dbc535bac63b119ae2cd8b01efd
tsemkaloalena <tsemkaloalena@gmail.com>
2022-10-02 19:43:24 +0300

    Method recordWord is written

--- FieldOfDreams/src/main/java/Game.java
+++ FieldOfDreams/src/main/java/Game.java
@@ -1,3 +1,15 @@
-public class Game {
+import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+	@Getter
+	private final List<Letter> word = new ArrayList<>();
+
+	public void recordWord(String enteredWord) {
+		for (Character character : enteredWord.toCharArray()) {
+			this.word.add(new Letter(character));
+		}
+	}
 }
