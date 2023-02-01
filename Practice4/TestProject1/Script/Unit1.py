054377a1649d269136134bb1c6520b1fade1eae6
tsemkaloalena <tsemkaloalena@gmail.com>
2023-02-01 08:18:56 +0300

    pr4

---
+++ Practice4/TestProject1/Script/Unit1.py
@@ -0,0 +1,28 @@
+﻿def start_application():
+  app = TestedApps.Items[0]
+  app.Params.SimpleParams.CommandLineParameters = "NotOpenApp"
+  p = app.Run()
+  for i in range(10):
+    count()
+  
+
+def click_number_by_name(btn_name):
+  btn = Sys.Process("Microsoft.WindowsCalculator").UIAObject("Калькулятор").UIAObject("LandmarkTarget").UIAObject("Числовая_панель").UIAObject(btn_name)
+  btn.Click()
+
+def click_operator_by_name(btn_name):
+  btn = Sys.Process("Microsoft.WindowsCalculator").UIAObject("Калькулятор").UIAObject("LandmarkTarget").UIAObject("Стандартные_операторы").UIAObject(btn_name)
+  btn.Click()
+  
+def count():
+  click_operator_by_name("Минус")
+  click_number_by_name("Два")
+  click_number_by_name("Восемь")
+  click_operator_by_name("Равно")
+  
+def close_application():
+  app = TestedApps.Items[0]
+  app.Close()
+  Delay(2000)
+  if p.Exists:
+    app.Terminate()
